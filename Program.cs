e2fe8ddb3e2fbc8bbc9b935bdeb43b3fb7a30ad4
Toni <89582846+Noccis@users.noreply.github.com>
2022-12-13 09:35:29 +0100

    added ui information for swagger

--- Program.cs
+++ Program.cs
@@ -6,12 +6,13 @@ using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
+// Roberts database
 builder.Services.Configure<PlanetsMongoDBSettings>(builder.Configuration.GetSection("MongoDBPlanets"));
 builder.Services.AddSingleton<PlanetMongoDBService>();
-
-
+// Susannas database
 builder.Services.Configure<InspectionDBSettings>(builder.Configuration.GetSection("TrainingDB"));
 builder.Services.AddSingleton<InspectionsDBService>();
+// Tonis database+
 builder.Services.Configure<ThoughtsDatabaseSettings>(
 builder.Configuration.GetSection("ThoughtsMongoDB")); 
 builder.Services.AddSingleton<ThoughtService>();
@@ -20,6 +21,18 @@ builder.Services.AddSingleton<ThoughtService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Group 2 API",
+        Description = "School project in AU21 from ItHögskolan Stockholm",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "John Doe",
+            Url = new Uri("https://johndoe.com/contact")
+        }
+    });
       var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     }
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Students:
 Susanna Johansson
 Robert Vatran
 Malcolm Lennartsson
-Toni Löf: ThoughtController.cs, ThoughtsDatabaseSettings.cs, Thought.cs, ThoughtService.cs, some code in appsettings.json and in program.cs
+Toni Löf: ThoughtController.cs, ThoughtsDatabaseSettings.cs, Thought.cs, Comment.cs, ThoughtService.cs, some code in appsettings.json and in program.cs
