2da82a902968438e58fd7264046592a926922dfa
Toni <89582846+Noccis@users.noreply.github.com>
2022-12-15 10:15:35 +0100

    added required to Comment

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,13 +1,19 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Grupp2.Models;
 
+#pragma warning disable CS1591
 public class Comment 
 {
+    [Required]
     public string Body { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+    public string Email { get; set; } = string.Empty;
 
+    [Required]
     public string Author { get; set; } = null!;
 }
+#pragma warning restore CS1591
